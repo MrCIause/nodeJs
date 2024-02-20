e3ab585e389aa4f61511edd06125d9c7d431872c
Adi Shcherbakov <104269635+MrCIause@users.noreply.github.com>
2024-02-20 14:13:58 +0200

    commit

--- server.js
+++ server.js
@@ -2,3 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
+
+app.get("/", (req, res) => {
+  // gets the root path and prints hello world.
+  // res.status(200).send("Hello World!"); // 200 is the status code for OK. // sends the info to the user.
+  res.status(500).json({ message: "Json message" }); // sends the json to the user.
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
